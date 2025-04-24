b8e6081c118a78e82859761cad58d59187a57cb8
i-archanasenthil <i.archanasenthil@gmail.com>
2025-04-23 22:14:32 -0600

    LDA Topic Modelling

--- text_analysis.py
+++ text_analysis.py
@@ -1,11 +1,10 @@
-import numpy as np
-import pandas as pd
+
 import spacy
-import nltk
 import gensim.corpora as corpora
 from gensim.models import CoherenceModel, LdaModel
 import pyLDAvis
 import pyLDAvis.gensim_models as gensimvis
+from gensim.models import TfidfModel
 import os
 import spacy.cli
 #spacy.cli.download("en_core_web_sm")
@@ -15,16 +14,15 @@ document = []
 
 file_paths = [f"data/{file}" for file in files]
 
-for path in file_paths:
-    with open(path, 'r') as f:
-        document.append(f.read())
+with open(f"data/1 - A Game of Thrones.txt", 'r') as f:
+    document = f.read()
 
 #spacy NLP model
 nlp = spacy.load("en_core_web_sm")
 nlp.max_length = 20_000_000
 
 from spacy.lang.en.stop_words import STOP_WORDS as stopwords
-custom_stopwords = {"and", "like"}
+custom_stopwords = {"and", "like", "say", "come", "know", "look","ask", "page", "go", "want"}
 stop_words = stopwords | custom_stopwords
 
 def process_text(text):
@@ -35,9 +33,65 @@ def process_text(text):
     doc = nlp(text.lower())
     return [token.lemma_ for token in doc if token.is_alpha and token.text not in stop_words]
 
-document = str(document)
-print(type(document))
-token = process_text(document)
-print(len(token))
 
+def compute_coherence(lda_model, texts, dictionary):
+    """
+    Compute the the coherence score for the trained LDA model
+    c_v is the metric for measuring the semantic similarity and co-occurence of those words
+    """
+    coherence_model_lda = CoherenceModel(model = lda_model, texts = texts, dictionary = dictionary, coherence = 'c_v')
+    return coherence_model_lda.get_coherence()
+
+
+document = [line for line in text.split('\n') if line.strip()]
+documents = [process_text(doc) for doc in document]
+
+texts = documents
+id2word = corpora.Dictionary(texts)
+id2word.filter_extremes(no_below=5, no_above=0.5)
+corpus = [id2word.doc2bow(text) for text in texts]
+
+tfidf = TfidfModel(corpus)
+corpus_tfidf = tfidf[corpus]
+
+#Identifying the best number of topics and the best corpus based on the coherence score and no overlapping topics
+scores = []
+for k in range(2, 21):
+    model = LdaModel(corpus=corpus_tfidf, id2word=id2word, num_topics=k, passes=10)
+    coherencemodel = CoherenceModel(model=model, texts=texts, dictionary=id2word, coherence='c_v')
+    score = coherencemodel.get_coherence()
+    scores.append((k, score))
+
+# Print best topic number
+best = max(scores, key=lambda x: x[1])
+print(f"Best num_topics: {best[0]} with coherence score: {best[1]}")
+
+#Identifying the top 5 to find a model with distinct topics
+top_5 = sorted(scores, key=lambda x: x[1], reverse=True)[:5]
+# Print top 5 results
+for rank, (num_topics, coherence) in enumerate(top_5, start=1):
+    print(f"Rank {rank}: num_topics = {num_topics}, coherence score = {coherence:.4f}")
+
+#running the final model with best param
+lda_model = LdaModel(
+    corpus = corpus,
+    id2word = id2word,
+    num_topics = 4,
+    passes = 10,
+    alpha='asymmetric', 
+    eta='auto'
+)
+for idx, topic in lda_model.print_topic(-1):
+    print(f"Topic #{idx}: {topic}")
+
+
+if __name__ == "__main__":
+    import multiprocessing
+    multiprocessing.freeze_support()
+    coherence_lda = compute_coherence(lda_model, texts, id2word)
+    print('\nCoherence Score: ', coherence_lda)
+
+# Visualize the LDA model
+vis = gensimvis.prepare(lda_model, corpus, id2word)
+pyLDAvis.display(vis)
 
